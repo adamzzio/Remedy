69a7227f575c61dbef5a2fc5ca265638e56d2ea1
adamzzio <adam.maurizio69@gmail.com>
2023-09-29 10:23:32 +0700

    revisi monev internal 2 - add file tambahan

---
+++ static/home/js/language.js
@@ -0,0 +1,8 @@
+// Hapus seluruh array langs kecuali untuk bahasa Indonesia
+var langs = [['Bahasa Indonesia',['id-ID']]];
+
+let select_dialect = document.querySelector('#select_dialect');
+
+// Hapus loop untuk menambahkan opsi bahasa pada select_language
+
+// Hapus function updateCountry
